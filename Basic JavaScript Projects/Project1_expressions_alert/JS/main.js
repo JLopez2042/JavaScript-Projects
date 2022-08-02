05e06d0e8e0690ea138a0e0b2eb4ced526aab7c8
JLopez2042 <jacob.lopez.2042@gmail.com>
2022-08-02 11:04:59 -0600

    Project2_functions assignment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -8,6 +8,6 @@ document.write(sent1+sent2);
 function My_First_Function() {              //Defining a function and naming it
     var str = "This is the button text!";   //Defining a variable and giving it
                                             //a string value
-    document.getElementById("Button_Text") .innerHTML = str;    //Putting the value
-}                                           //of the variable into the HTML elementFromPoint
+    document.getElementById("Button_Text") .innerHTML = str;   //Putting the value
+}                                           //of the variable into the  HTML elementFromPoint
                                             //with the "Button_Text" id
---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+
+function myFunction() {                 //Defining a function and naming it
+    var sentence = "I am learning" ;    //Defining a variable
+    sentence += ", a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence; //Putting the value
+}                                           //of the variable into the  HTML elementFromPoint
